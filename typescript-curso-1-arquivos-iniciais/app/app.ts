d75cd7f47ac7eaa18323ad2c5dded53e2879dcd3
tiagoOliveira <89092378+tiago19deoliveira@users.noreply.github.com>
2024-04-10 09:48:25 -0300

    Delete typescript-curso-1-arquivos-iniciais directory

--- typescript-curso-1-arquivos-iniciais/app/app.ts
+++
@@ -1,6 +0,0 @@
-import { Negociacao, Negociacao, Negociacao } from "./models/negociacao"; 
-
-const Negociacao = new Negociacao(new Date(),10,100);
-console.log(Negociacao);
-Negociacao.quantidade = 1000;
-console.log(Negociacao.volume);
