bb80d9f5e74f7237b8646e45fadf5e5b8613f6cf
tiago19deoliveira <tiago18deoliveira@gmail.com>
2024-05-27 12:00:59 -0300

    refactor: aplicando refatoração do uso de atributos de acesso

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,12 +1,10 @@
 export class Negociacao{
-     private _data: Date;
-     private _valor: number;
-     private _quantidade:number ;
 
-    constructor(data: Date,valor: number,quantidadae: number){
-        this._data = data;
-        this._valor = valor;
-        this._quantidade = quantidadae;
+    constructor(
+      private _data: Date,
+      private _valor: number,
+      private _quantidade: number){
+ 
     }
 
     get data(): Date {
